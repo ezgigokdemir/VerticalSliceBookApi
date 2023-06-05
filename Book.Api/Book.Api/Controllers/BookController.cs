e8c2d0a6de1da394289c907d6f61f1331f066941
Ezgi Gökdemir <38529111+ezgigokdemir@users.noreply.github.com>
2023-06-05 11:26:42 +0300

    Add files via upload

---
+++ Book.Api/Book.Api/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Book.Api.Features.Book.Commands;
+using Book.Api.Features.Book.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Book.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public ActionResult<AddBookResponse> AddBook([FromBody] AddBookCommand request) 
+        {
+            
+            var response = _mediator.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public ActionResult<GetBookByIdResponse> GetBookById([FromBody] GetBookByIdCommand request)
+        {
+
+            var response = _mediator.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
